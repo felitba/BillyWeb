989561288c8fea91de3fa69b6d5da70809e24157
felitba <feli@itba.edu.ar>
2024-11-13 12:51:22 -0300

    building a basic home view

---
+++ src/components/Download.vue
@@ -0,0 +1,19 @@
+
+<template>
+  <div>
+    <button v-if="platforms === 'android'" @click="downloadApp('android')">Descargar para Android</button>
+    <button v-if="platforms === 'ios'" @click="downloadApp('ios')">Descargar para iOS</button>
+  </div>
+</template>
+
+<script setup>
+const props = defineProps(['platforms']);
+
+function downloadApp(platform) {
+  if (platform === 'android') {
+    window.open('link_to_android_app', '_blank');
+  } else if (platform === 'ios') {
+    window.open('link_to_ios_app', '_blank');
+  }
+}
+</script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,13 @@
 <script setup>
+import Download from '@/components/Download.vue';
 </script>
 
 <template>
   <main>
+    <h1>Billy</h1>
+    <p>Billy es un gestor de movimientos, ayudando a gestionar tus tareas de manera eficiente.</p>
+    
+    <Download platforms="android" />
+    <Download platforms="ios" />
   </main>
 </template>
