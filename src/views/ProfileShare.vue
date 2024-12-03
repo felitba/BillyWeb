30c6164f9a240daf029422449b992932aec6c5c1
Matías Leporini <matilepo03@gmail.com>
2024-12-02 23:34:39 -0300

    Road to profile sharing

--- src/views/ProfileShare.vue
+++ src/views/ProfileShare.vue
@@ -4,6 +4,21 @@
     </div>
   </template>
   
+  <script>
+  export default {
+    mounted() {
+      const urlParams = new URLSearchParams(window.location.search);
+      const invitationId = urlParams.get('invitationId');
+  
+      if (invitationId) {
+        window.location.href = `billy://profile?invitationId=${invitationId}`;
+      } else {
+        document.body.innerHTML = '<h1>Invalid invitation link.</h1>';
+      }
+    }
+  }
+  </script>
+  
   <style scoped>
   .profile-share-container {
     text-align: center;
